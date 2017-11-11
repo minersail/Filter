c421f740bafea906569878d30c717aa634c61bd8
minersail <jkwoo1234@gmail.com>
2017-11-11 18:55:05 -0500

    uh it works i guess

--- content.js
+++ content.js
@@ -1,1 +1,13 @@
-$("img").attr("src", "https://www.cs.umd.edu/users/pugh/fireEating.jpg");
+var profanityList = ["fuck", "shit", "Ass", "cunt", "bitch", "Tits", "pussy", "hobknocker", "fag", "faggot", "fucker", "fucking", "asshole", "dicks", "dick", "slut", "Hoes", "skank", "dick", "bitchass", "Wanker", "Bloody", "clit", "cock", " cum ", "douchbag", "dike", "douche", "dumbass", "motherfucker"];
+
+$("p, dl, dd, dt").each(function()
+{
+	var replaced = $(this).text();
+	
+	for (var i = 0; i < profanityList.length; i++)
+	{
+		replaced = replaced.replace(profanityList[i].toLowerCase(), "HARASSMENT IS NEVER OKAY.");
+		replaced = replaced.replace(profanityList[i].substring(0, 1).toUpperCase() + profanityList[i].toLowerCase().substring(1), "HARASSMENT IS NEVER OKAY.");
+	}
+	$(this).text(replaced);
+});
